e9233ab496ffb8f26604af0c5b356d0162b69fa6
starbvuks <starbvuks@gmail.com>
2022-08-09 14:32:07 +0530

    firsst

---
+++ eth101/HelloWorld.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.3;
+
+// A contract in the sense of Solidity is a collection of code
+// (its functions) and data (its state) that resides at a specific
+// address on the Ethereum blockchain.
+
+contract HelloWorld {
+    string public greeting = "Hello Wolrd";
+}
