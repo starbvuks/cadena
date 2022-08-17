25b478bd7ac777c58ef048355f42774b465caf8b
starbvuks <starbvuks@gmail.com>
2022-08-17 23:06:10 +0530

    task updates

--- final-projs/proj1/contracts/TodoList.sol
+++ final-projs/proj1/contracts/TodoList.sol
@@ -2,6 +2,22 @@ pragma solidity ^0.8.9;
 
 contract TodoList {
     uint256 taskCount = 0;
-    string content;
-    bool done;
+
+    struct Task {
+        uint256 id;
+        string content;
+        bool done;
+    }
+
+    mapping(uint256 => Task) public tasks;
+
+    constructor() public {
+        createTask("first task");
+    }
+
+    function createTask(string memory _content) public {
+        taskCount++;
+        // task num (task-count) has content and is set completed = false
+        tasks[taskCount] = Task(taskCount, _content, false);
+    }
 }
